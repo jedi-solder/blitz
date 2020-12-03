8ad52a8e530d0e5ced8c127bdc93fd40ff128051
jedi-solder <soldatovvn91@gmail.com>
2020-12-03 11:19:08 +0500

    gulp, sass, sasslint, normalize is ready

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const gulpStylelint = require('gulp-stylelint');
+
+function style() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
+}
+
+function lintCss() {
+    return gulp
+        .src('./scss/**/*.scss')
+        .pipe(gulpStylelint({
+            reporters: [
+                {
+                    formatter: 'string',
+                    console: true
+                }
+            ]
+        }));
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.lintCss = lintCss;
+exports.watch = watch;
